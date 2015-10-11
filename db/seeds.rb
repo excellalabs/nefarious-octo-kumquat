974b84df11fc42796dcb8251c56acdaf28350219
Evan Botello <botellde@gmail.com>
2015-10-11 16:18:21 +0000

    Add questions in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Question.create(text: 'Is Excella hiring?', answer: 'Yes')
+Question.create(text: 'What is a good programming language?', answer: 'Ruby')
